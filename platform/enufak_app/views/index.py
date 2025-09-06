66f3225fdff644d598371be044355600dd758125
Ekin Ilter Varli <ekinnos@tutanota.com>
2025-09-06 23:33:04 +0300

    Added pagination

--- platform/enufak_app/views/index.py
+++ platform/enufak_app/views/index.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from enufak_app.models import Ilan
+from django.core.paginator import Paginator
+
 
 def index(request):
-    ilanlar = Ilan.objects.filter(aktif=True).order_by('-id')[:6]
+    ilan_liste = Ilan.objects.filter(aktif=True).order_by('-id')
+    paginator = Paginator(ilan_liste, 6)
+    
+    page_number = request.GET.get('sayfa')
+    ilanlar = paginator.get_page(page_number)
+
     return render(request, 'app/kesfet.jinja', context={
         'ilanlar':ilanlar,
     })
