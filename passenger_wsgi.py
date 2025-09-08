b433e1f383f85cb2631457136fb7b31868e75d10
Ekin Ilter Varli <ekinnos@tutanota.com>
2025-09-08 23:53:30 +0300

    Added passenger wsgi

---
+++ passenger_wsgi.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+# Django projenizin yolunu belirleyin
+sys.path.insert(0, '/home/enufakco/dev.enufak.com/')  # 'username' yerine CPanel kullanıcı adınızı ve projenizin doğru yolunu yazın.
+
+# Django ayarlarını yükleyin
+os.environ['DJANGO_SETTINGS_MODULE'] = 'enufak.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
