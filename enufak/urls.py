c2321d6f81f4a21b9067e70ad410288cf197e445
Ekin Ilter Varli <ekinnos@tutanota.com>
2025-09-09 00:12:27 +0300

    Some changes

--- enufak/urls.py
+++ enufak/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+import os
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('enufak_core.urls')),
@@ -34,4 +37,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
