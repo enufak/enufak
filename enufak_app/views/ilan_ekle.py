b29b475b74143d456894cefa7929225ac21da927
Ekin Ilter Varli <ekinnos@tutanota.com>
2025-09-16 00:54:30 +0300

    .

--- enufak_app/views/ilan_ekle.py
+++ enufak_app/views/ilan_ekle.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from enufak_app.forms import IlanEkleForm
+from django.contrib import messages
 
 @login_required
 def ilan_ekle(request):
@@ -10,6 +11,7 @@ def ilan_ekle(request):
             ilan = form.save(commit=False)
             ilan.ilan_sahibi = request.user 
             ilan.save()
+            messages.success(request, 'İlanınız başarıyla oluşturuldu. Sistem tarafından onaylandıktan sonra profilinize eklenecektir.')
             return redirect("profil")
     else:
         form = IlanEkleForm()
