fece91296b3aff3f3e162ece8a96f4867524fee3
Ekin Ilter Varli <ekinnos@tutanota.com>
2025-11-08 20:15:57 +0300

    Update: SQLite to PostgreSQL

--- enufak/settings.py
+++ enufak/settings.py
@@ -134,10 +134,15 @@ WSGI_APPLICATION = 'enufak.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'enufak_db',
+        'USER': 'enufak_user',
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
